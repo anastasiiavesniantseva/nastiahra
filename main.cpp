c78bdcaa4c16760d0dec9225b4a44f5d0fce349e
Vesniantsev <alex.1982.1959@gmail.com>
2026-04-26 14:25:09 +0200

    zmeny kodu

--- main.cpp
+++ main.cpp
@@ -1,8 +1,41 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
-    cout << "Ahoj" << endl;
-    return 0;
-}
+    string jmeno;
+    int zivoty = 10;
+    int zlato = 20;
+    int utok = 3;
+    int volba;
+
+    cout << "=== MANAEATER ===" << endl;
+    cout << "Vitej v textove hre!" << endl;
+    cout << "Zadej jmeno hrace: ";
+    cin >> jmeno;
+
+    cout << endl;
+    cout << "Ahoj, " << jmeno << "!" << endl;
+    cout << "Tvuj ukol je projit cestou, porazit monstra" << endl;
+    cout << "a na konci premoci hlavniho bosse Manaeatera." << endl;
+
+    cout << endl;
+    cout << "Tvoje statistiky:" << endl;
+    cout << "Zivoty: " << zivoty << endl;
+    cout << "Zlato: " << zlato << endl;
+    cout << "Utok: " << utok << endl;
+
+    cout << endl;
+    cout << "Stojis na zacatku temne cesty." << endl;
+    cout << "Co chces udelat?" << endl;
+    cout << "1 - Jit do vesnice" << endl;
+    cout << "2 - Jit do lesa" << endl;
+    cout << "3 - Zobrazit statistiky" << endl;
+    cout << "Tvoje volba: ";
+    cin >> volba;
+
+    if (volba == 1)
+    {
+        cout << "Prichazis do vesnice." << endl;
+    }
