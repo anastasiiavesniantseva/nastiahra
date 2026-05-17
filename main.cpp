87fda87b2071c1fe97c7b83ebdcf127af42b020c
Vesniantsev <alex.1982.1959@gmail.com>
2026-05-17 22:13:55 +0200

    dal

--- main.cpp
+++ main.cpp
@@ -88,3 +88,69 @@ int main()
         cout << "Zlato: " << zlato << endl;
         cout << "Utok: " << utok << endl;
     }
+    cout << endl;
+
+    cout << "Pokracujes dal cestou..." << endl;
+
+    cout << "Narazil jsi na jeskyni." << endl;
+
+    cout << endl;
+
+    cout << "1 - Vstoupit do jeskyne" << endl;
+
+    cout << "2 - Odejit" << endl;
+
+    cin >> volba;
+
+    if (volba == 1)
+    {
+
+        cout << endl;
+
+        cout << "Objevili se 2 pavouci!" << endl;
+
+        int spider1 = 5;
+
+        int spider2 = 5;
+
+        while ((spider1 > 0 || spider2 > 0) && zivoty > 0)
+        {
+
+            cout << endl;
+
+            cout << "Spider 1 HP: " << spider1 << endl;
+
+            cout << "Spider 2 HP: " << spider2 << endl;
+
+            cout << "Tvoje HP: " << zivoty << endl;
+
+            cout << "1 - Utok na spider 1" << endl;
+
+            cout << "2 - Utok na spider 2" << endl;
+
+            cin >> volba;
+
+            if (volba == 1 && spider1 > 0)
+            {
+
+                spider1 -= utok;
+
+                cout << "Zasahl jsi spider 1." << endl;
+            }
+
+            else if (volba == 2 && spider2 > 0)
+            {
+
+                spider2 -= utok;
+
+                cout << "Zasahl jsi spider 2." << endl;
+            }
+
+            if (spider1 > 0 || spider2 > 0)
+            {
+
+                zivoty -= 3;
+
+                cout << "Pavouci utoci!" << endl;
+            }
+        }
